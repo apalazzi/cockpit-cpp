27f74ab3be418b4ea2f2f1a8c39afe794c428e71
Andrea Palazzi <palazziandrea@yahoo.it>
2017-02-08 21:33:50 +0100

    Updated license headers.

--- include/usb_cockpit.h
+++ include/usb_cockpit.h
@@ -1,20 +1,20 @@
 /*
     Copyright 2017 Andrea Palazzi <palazziandrea@yahoo.it>
 
-    This file is part of ed-autopilot-cockpit.
+    This file is part of cockpit-c++.
 
-    ed-autopilot-cockpit is free software: you can redistribute it and/or modify
+    cockpit-c++ is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    ed-autopilot-cockpit is distributed in the hope that it will be useful,
+    cockpit-c++ is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with ed-autopilot-cockpit.  If not, see <http://www.gnu.org/licenses/>.
+    along with cockpit-c++.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef __USB_COCKPIT_H
--- src/syscalls.c
+++ src/syscalls.c
@@ -1,22 +1,9 @@
-/*
-    Copyright 2017 Andrea Palazzi <palazziandrea@yahoo.it>
-
-    This file is part of ed-autopilot-cockpit.
-
-    ed-autopilot-cockpit is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    ed-autopilot-cockpit is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with ed-autopilot-cockpit.  If not, see <http://www.gnu.org/licenses/>.
-
- */
+//
+// This file is part of the µOS++ III distribution.
+// Parts of this file are from the newlib sources, issued under GPL.
+// Copyright (c) 2014 Liviu Ionescu
+//
+// modified by Andrea Palazzi <palazziandrea@yahoo.it> 2017
 
 #include <syscalls.h>
 
